e9ede72c154291f180a3d33a3c2804158bc7cce6
AoiKagase <datumoujidai@gmail.com>
2018-11-17 16:16:51 +0900

    First Commit

---
+++ FieldLabelAttribute.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+public class FieldLabelAttribute : PropertyAttribute
+{
+    public readonly string text;
+
+    public FieldLabelAttribute(string text)
+    {
+        this.text = text;
+    }
+}
---
+++ FieldLabelDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(FieldLabelAttribute))]
+internal sealed class FieldLabelDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        FieldLabelAttribute fLabel = (FieldLabelAttribute) attribute;
+        EditorGUI.PropertyField(position, property, new GUIContent(fLabel.text));
+    }
+}
+
